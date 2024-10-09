bf4c9ea465830583fda7302b65b4896e35133442
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-10-09 11:42:22 +0100

    :sparkles: (Status) add in progress status

--- tests/shared/unit/domain/test_status.py
+++ tests/shared/unit/domain/test_status.py
@@ -4,3 +4,6 @@ from src.task.domain.value_objects.status import Status
 class TestStatus:
     def test_done_status(self):
         assert Status.DONE.value == "done"
+
+    def test_in_progress_status(self):
+        assert Status.IN_PROGRESS.value == "in-progress"
