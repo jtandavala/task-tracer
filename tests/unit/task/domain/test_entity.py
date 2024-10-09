04f3d123db980d5016c0624dc780549b9cf525e7
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-10-09 16:32:49 +0100

    :sparkles: (Task) throw exception when create Task with invalid description

--- tests/unit/task/domain/test_entity.py
+++ tests/unit/task/domain/test_entity.py
@@ -2,17 +2,19 @@ import unittest
 from datetime import datetime
 from uuid import UUID
 
+from pydantic import ValidationError
+
 from src.task.domain.entity import Task
 from src.task.domain.value_objects.status import Status
 
 
 class TestTaskUnit(unittest.TestCase):
     def test_create_task(self):
-        task = Task(description="test", status=Status.TODO)
+        task = Task(description="test", status=Status.IN_PROGRESS)
 
         self.assertTrue(isinstance(task.id, UUID))
         self.assertEqual(task.description, "test")
-        self.assertEqual(task.status, Status.TODO)
+        self.assertEqual(task.status, Status.IN_PROGRESS)
         self.assertTrue(isinstance(task.created_at, datetime))
         self.assertTrue(isinstance(task.updated_at, datetime))
 
@@ -22,3 +24,8 @@ class TestTaskUnit(unittest.TestCase):
         self.assertTrue(isinstance(task.id, UUID))
         self.assertEqual(task.description, "Test")
         self.assertEqual(task.status, Status.TODO)
+
+    def test_throw_exception_when_invalid_description(self):
+        with self.assertRaises(ValidationError) as e:
+            Task(status=Status.DONE)
+        self.assertTrue(isinstance(e.exception, ValidationError))
