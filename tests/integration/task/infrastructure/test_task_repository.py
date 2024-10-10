0c0e6fafde8dc40da52af166c657010461d56803
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-10-10 13:29:03 +0100

    :sparkles: (Task Repository) list tasks with in-progress stattus

--- tests/integration/task/infrastructure/test_task_repository.py
+++ tests/integration/task/infrastructure/test_task_repository.py
@@ -165,3 +165,20 @@ class TestTaskAlchemyRepository:
         assert tasks.items[0].status == Status.DONE
         assert tasks.items[0].created_at == task2.created_at
         assert tasks.items[0].updated_at == task2.updated_at
+
+    def test_list_task_with_in_progress_status(self, connection, migrations):
+        task1 = Task(description="task 1")
+        task2 = Task(description="task 2", status=Status.IN_PROGRESS)
+
+        repository = TaskSqliteRepository(connection)
+        repository.save(task1)
+        repository.save(task2)
+
+        tasks = repository.list(filter=Status.IN_PROGRESS.value)
+
+        assert len(tasks.items) == 1
+        assert isinstance(tasks.items[0].id, UUID) is True
+        assert tasks.items[0].description == task2.description
+        assert tasks.items[0].status == Status.IN_PROGRESS
+        assert tasks.items[0].created_at == task2.created_at
+        assert tasks.items[0].updated_at == task2.updated_at
