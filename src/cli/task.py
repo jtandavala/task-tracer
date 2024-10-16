d2d3bb87935ced7214176faedff390f26283737d
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-10-16 19:29:03 +0100

    :sparkles: (TaskCli) find task by id

--- src/cli/task.py
+++ src/cli/task.py
@@ -1,6 +1,7 @@
 from src.task.commands.concrete.add_task_command import AddTaskCommand
 from src.task.domain.value_objects.dto import TaskDto
 from src.task.invoker.task_invoker import TaskInvoker
+from src.task.queries.task_query_by_id import TaskQueryById
 from src.task.receiver.task_receiver import TaskReceiver
 
 
@@ -12,3 +13,10 @@ class TaskCli:
     def create_task(self, task_dto: TaskDto):
         add_task_command = AddTaskCommand(TaskReceiver(self.connection), task_dto)
         return self.invoker.execute_command(add_task_command)
+
+    def find_by_id(self, id: int):
+        try:
+            query = TaskQueryById(self.connection, id)
+            return query.execute()
+        except Exception as e:
+            return e.value
--- tests/cli/test_task.py
+++ tests/cli/test_task.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 from src.cli.task import TaskCli
+from src.task.domain.entity import Task
+from src.task.domain.value_objects.status import Status
 
 
 class TestTaskCli:
@@ -8,3 +12,26 @@ class TestTaskCli:
         task_id = task.create_task(task_dto)
 
         assert isinstance(task_id, int) is True
+
+    def test_find_task_by_id(self, connection, migrations):
+        cli = TaskCli(connection)
+        task_id = cli.create_task({"description": "test"})
+
+        task: Task = cli.find_by_id(task_id)
+
+        assert isinstance(task.id, int) is True
+        assert task.id == task_id
+        assert task.description == "test"
+        assert task.status == Status.TODO
+        assert isinstance(task.created_at, datetime) is True
+        assert isinstance(task.updated_at, datetime) is True
+
+    # def test_can_update_task(self, connection, migrations):
+    #     task_dto = {"description": "test"}
+    #     cli = TaskCli(connection)
+    #     task_id = cli.create_task(task_dto)
+
+    #     task_dto["id"] = task_id
+    #     task_dto["description"] = "test updated"
+
+    #     cli.update_task(task_dto)
